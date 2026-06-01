02f5849bc4b7edef4e3ecc2397284d3343052c33
mooton <mooton100>
2026-06-01 17:41:35 +0700

    edit setting preview

--- src/setting.ts
+++ src/setting.ts
@@ -16,12 +16,13 @@ export class ReferenceSettingTab extends PluginSettingTab {
     containerEl.empty();
 
     // Introduction
-    containerEl.createEl('h2', {text: 'Reference Link'});
-    containerEl.createEl(
-        'p', {text: 'Create beautiful reference cards from URLs.'});
+    new Setting(containerEl)
+        .setName('Reference Link')
+        .setHeading()
+        .setDesc('A plugin to create reference cards from URLs.');
 
     // Usage instructions
-    containerEl.createEl('h3', {text: 'How to use'});
+    new Setting(containerEl).setName('Usage').setHeading();
 
     const pre = containerEl.createEl('pre', {cls: 'reference-example-code'});
 
@@ -32,18 +33,6 @@ OpenAI : https://openai.com
 Google : https://google.com
 \`\`\``);
 
-    // Quick insert instructions
-    containerEl.createEl('h3', {text: 'Commands'});
-
-    const commandList = containerEl.createEl('ul');
-
-    commandList.createEl('li', {
-      text:
-          '/Reference - Insert a reference block template at the cursor position.'
-    });
-
-    // Card preview
-    containerEl.createEl('h3', {text: 'Card preview'});
     const preview = containerEl.createDiv({cls: 'reference-container'});
 
     const examples = [
@@ -59,6 +48,16 @@ Google : https://google.com
       const img = card.createEl('img', {cls: 'reference-image'});
 
       img.src = 'https://placehold.co/600x300';
+    });
+
+    // Quick insert instructions
+    new Setting(containerEl).setName('Commands').setHeading();
+
+    const commandList = containerEl.createEl('ul');
+
+    commandList.createEl('li', {
+      text:
+          '/Reference - Insert a reference block template at the cursor position.'
     });
   }
 }
